f2b76bff4d9b0d7990d3a586cbec0d1f464b895e
Abhay Joshi <sychonet@gmail.com>
2019-12-31 11:51:01 +0530

    Fixed a typo

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func main() {
 func run() {
 	fmt.Printf("Running %v\n", os.Args[2:])
 
-	cmd := exec.Command("proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+	cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
 	cmd.Stdin = os.Stdin
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
