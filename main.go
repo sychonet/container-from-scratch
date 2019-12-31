46eb13a579fddb34a103d6e29c5cec36b09bd26e
Abhay Joshi <sychonet@gmail.com>
2019-12-31 18:25:01 +0530

    Added functionality for change groups

--- main.go
+++ main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -30,9 +33,11 @@ func run() {
 	//Namespacing the hostname
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		//Cloneflags are parameters that will be passed on clone syscall. Clone is what actually creates a new process.
-		//CLONE_NEWUTS is the namespace
+		//CLONE_NEWUTS / CLONE_NEWPID / CLONE_NEWNS is the namespace
 		//UTS : Unix Timesharing System
-		Cloneflags: syscall.CLONE_NEWUTS,
+		//PID : Process ID
+		//NS : Name Space
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 
 	must(cmd.Run())
@@ -40,6 +45,8 @@ func run() {
 
 func child() {
 	fmt.Printf("Running %v\n", os.Args[2:])
+
+	cg()
 
 	cmd := exec.Command(os.Args[2], os.Args[3:]...)
 	cmd.Stdin = os.Stdin
@@ -49,8 +56,26 @@ func child() {
 	must(syscall.Sethostname([]byte("container")))
 	must(syscall.Chroot("/home/ubuntu/container-from-scratch/container-root"))
 	must(os.Chdir("/"))
+	must(syscall.Mount("proc", "proc", "proc", 0, ""))
+	must(syscall.Mount("something", "mytemp", "tempfs", 0, ""))
 
 	must(cmd.Run())
+
+	must(syscall.Unmount("proc", 0))
+	must(syscall.Unmount("mytemp", 0))
+}
+
+func cg() {
+	cgroups := "/sys/fs/cgroup/"
+
+	mem := filepath.Join(cgroups, "memory")
+	os.Mkdir(filepath.Join(mem, "containerscratch"), 0755)
+	must(ioutil.WriteFile(filepath.Join(mem, "containerscratch/memory.limit_in_bytes"), []byte("999424"), 0700))
+	must(ioutil.WriteFile(filepath.Join(mem, "containerscratch/memory.memsw.limit_in_bytes"), []byte("999424"), 0700))
+	must(ioutil.WriteFile(filepath.Join(mem, "containerscratch/notify_on_release"), []byte("1"), 0700))
+
+	pid := strconv.Itoa(os.Getpid())
+	must(ioutil.WriteFile(filepath.Join(mem, "containerscratch/cgroup.procs"), []byte(pid), 0700))
 }
 
 func must(err error) {
